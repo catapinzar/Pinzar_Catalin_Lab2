237143213053a06e39fcfaf960fe2d90ec6228bb
Cata Pinzar <catalin.pinzar@gmail.com>
2020-10-28 20:16:10 +0200

    lab4

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -57,7 +57,7 @@ namespace Pinzar_Catalin_Lab2.Controllers
                     books = books.OrderBy(b => b.Title);
                     break;
             }
-            int pageSize = 2;
+            int pageSize = 3;
             return View(await PaginatedList<Book>.CreateAsync(books.AsNoTracking(), pageNumber ??
            1, pageSize));
         }
